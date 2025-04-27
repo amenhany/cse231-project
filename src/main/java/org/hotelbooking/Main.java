ca3d3a06cdbf0f574e405d611c759a064eef9f73
Amen Hany <amen.hany.2810@gmail.com>
2025-04-27 16:05:51 +0300

    Added Gradle Integration with JavaFX Plugin

--- src/main/java/org/hotelbooking/Main.java
+++ src/main/java/org/hotelbooking/Main.java
@@ -1,9 +1,22 @@
 package org.hotelbooking;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.hotelbooking.core.*;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        Application.launch(args);
+    }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btOK = new Button("OK"); // create a button
+        Scene scene = new Scene(btOK, 200, 250); // create a scene WITH the button
+        stage.setTitle("MyJavaFX"); // Set the stage title
+        stage.setScene(scene); // Place the scene in the stage
+        stage.show();
     }
 }
