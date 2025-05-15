9720af029d6968124c7709ce2ecb25138a587457
Hazem <Hazemali01010@gmail.com>
2025-05-15 03:54:23 +0300

    Wady🔥

--- src/main/java/org/hotelbooking/controllers/Home.java
+++ src/main/java/org/hotelbooking/controllers/Home.java
@@ -1,4 +1,23 @@
 package org.hotelbooking.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
+
 public class Home {
+
+
+    @FXML
+    private AnchorPane contentArea;
+    @FXML
+    private void loadNewPane() {
+        try {
+            AnchorPane newPane = FXMLLoader.load(getClass().getResource("bookingsPane.fxml"));
+            contentArea.getChildren().setAll(newPane); // replaces current content
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
